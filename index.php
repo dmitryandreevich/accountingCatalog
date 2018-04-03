2c316e94b15b824ad5a4d3901174d87d64156b13
Dmitry Skripak <zverdiman9875@gmail.com>
2018-04-03 16:52:51 +0600

    Add pages

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
+
+    <?php include('layouts/header.php'); ?>
+
+    <div class="row">
+        <div class="col-md-12">
+            <main>
+                content
+            </main>
+        </div>
+    </div>
+	<div class="row">
+		<div class="col-md-12">
+			<footer>
+				footer
+			</footer>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,16 @@
+<header class="header">
+    <div class="row justify-content-between align-items-center header-block">
+        <div class="col-md-4">
+            <h2>Сайт того-то того</h2>
+        </div>
+        <div class="col-md-6 ">
+            <nav class="navigation">
+                <ul>
+                    <li><a href="">Главная</a></li>
+                    <li><a href="">Товары</a></li>
+                    <li><a href="">Рекламное агенство</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
