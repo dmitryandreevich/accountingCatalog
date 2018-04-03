8bfda548295bda7a0387591a968969edf22cee03
Dmitry Skripak <zverdiman9875@gmail.com>
2018-04-03 22:55:27 +0600

    Write CatalogModule

--- catalog.php
+++ catalog.php
@@ -1,13 +1,47 @@
 <?php
     include('layouts\head.php');
+    require __DIR__.'/core/CatalogModule.php';
 
+    $catalog = new core\CatalogModule();
+
+    if(isset($_POST['new'])) // если была нажата кнопка только для всех товаров
+        $catalog->setProductsQuality('new'); // устанавливаем тип продукта для выборки из базы
+    elseif(isset($_POST['old']))
+        $catalog->setProductsQuality('old');
 ?>
 <div class="container">
     <?php include('layouts\header.php'); ?>
+
     <div class="row">
         <div class="col-md-12">
             <main>
-                catalog
+                <div class="row justify-content-between">
+                    <div class="col-md-12">
+                        <form action="" method="post">
+                            <input type="submit" name="all" class="btn btn-success" value="Все товары">
+                            <input type="submit" name="new" class="btn btn-success" value="Новые товары">
+                            <input type="submit" name="old" class="btn btn-success" value="Б/У товары">
+                        </form>
+                    </div>
+                </div>
+                <table class="table">
+                    <tr>
+                        <th>#</th>
+                        <th>Имя</th>
+                        <th>Количество</th>
+                    </tr>
+                    <?php
+                       $products = $catalog->getProducts();
+                       foreach ($products as $product):
+                    ?>
+                       <tr>
+                           <td><?= $product['id'] ?></td>
+                           <td><?= $product['name'] ?></td>
+                           <td><?= $product['count'] ?></td>
+                       </tr>
+                    <?php endforeach; ?>
+                </table>
+
             </main>
         </div>
     </div>
---
+++ core/CatalogModule.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmitry Andreevich
+ * Date: 03.04.2018
+ * Time: 20:47
+ */
+
+namespace core;
+
+
+class CatalogModule
+{
+    protected $dbConn;
+    protected $connectInfo = [
+        'host' => 'localhost',
+        'dbname' => 'accounting',
+        'username' => 'root',
+        'password' => ''
+    ];
+    protected $productQuality = 'all';
+    public function __construct()
+    {
+        $this->dbConn = new \mysqli($this->connectInfo['host'],$this->connectInfo['username'],$this->connectInfo['password'], $this->connectInfo['dbname']);
+        if($this->dbConn->connect_error)
+            die('Connect error '.$this->dbConn->connect_error);
+        $this->createTableProducts();
+    }
+    public function __destruct()
+    {
+        $this->dbConn->close();
+    }
+    public function getProducts(){
+        $query = sprintf("SELECT id,name,count FROM products %s",  $this->productQuality == 'all' ? '' : 'WHERE quality="' . $this->productQuality. '"');
+        $qResult = $this->dbConn->query($query);
+        $products = array();
+        for($i = 0; $i < $qResult->num_rows; $i++)
+            array_push($products,$qResult->fetch_assoc());
+        return $products;
+    }
+    public function setProductsQuality($quality){
+        $this->productQuality = $quality;
+    }
+    private function createTableProducts(){
+        $qResult = $this->dbConn->query('SELECT id FROM products');
+        if(empty($qResult)){
+            $query = "CREATE TABLE products (
+                          id int(11) AUTO_INCREMENT,
+                          name varchar(255) NOT NULL,
+                          count int NOT NULL,
+                          quality varchar(15) NOT NULL,
+                          PRIMARY KEY  (id)
+                          )";
+            $this->dbConn->query($query);
+        }
+    }
+}
